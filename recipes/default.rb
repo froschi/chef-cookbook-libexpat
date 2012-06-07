105216eb4c1b9e97af12e264c632fee5b7689ba4
Thorsten Fischer <thorsten@froschi.org>
2012-06-07 20:42:40 +0200

    Collapsed lucid, precise case statement in recipe.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,7 @@
 packages = Array.new
 
 case node[:lsb][:codename]
-when "lucid"
-  packages |= %w/
-    libexpat1
-  /
-when "precise"
+when "lucid", "precise"
   packages |= %w/
     libexpat1
   /
